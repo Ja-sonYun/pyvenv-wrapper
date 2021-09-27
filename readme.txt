12a52d97d0d9606108ff4417f28b4bac625c45b4
Ja-sonYun <killa30867@gmail.com>
2021-09-27 14:47:10 +0900

    -

---
+++ readme.txt
@@ -0,0 +1,14 @@
+simple shell script wrapper of command `python venv -m <VENV_NAME>` with pyenv.
+
+`pyv new <VENV_NAME>` to create new virtual environment.
+
+`pyv ls` to list up all created virtual environments.
+
+`pyv activate <VENV_NAME>` or `pyv <VENV_NAME>` to activate virtual environment.
+
+`pyv remove <VENV_NAME>` to remove, `pyv deactivate` to deacitvate.
+
+To use with specific python version (pyenv), use below,
+`pyv new <VENV_NAME> <PYTHON_VERSION>`
+will backup current pyenv global and switch to specific version, and create virtualenv,
+after created, revert to previous pyenv global version.
---
+++ venv.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+export PYVENV_ROOT="$HOME/.pyvenv"
+
+function pyv() {
+    ENV_FILE=".pv"
+    TARGET_ENV=$PYVENV_ROOT/$2
+    SET_PYENV=false
+
+    case "$1" in
+        new | n )
+            if [ -d "$TARGET_ENV" ]; then
+                echo "env $2 already exists!"
+            else
+                if [ ! -z "$3" ] && command -v pyenv &> /dev/null; then
+                    pyenv global > $PYVENV_ROOT/$ENV_FILE
+                    pyenv global $3 || pyenv install $3 && pyenv global $3
+                    SET_PYENV=true
+                fi
+
+                echo "creating new virtual environment on $TARGET_ENV"
+                python3 -m venv $TARGET_ENV
+
+                if [ "$SET_PYENV" = true ]; then
+                    pyenv global `cat $PYVENV_ROOT/$ENV_FILE`
+                fi
+            fi;;
+        active | a )
+            if [ -d "$TARGET_ENV" ]; then
+                source $TARGET_ENV/bin/activate
+            else
+                echo "env $2 not found!"
+            fi;;
+        deactivate | d )
+            deactivate;;
+        remove | r )
+            if [ -d "$TARGET_ENV" ]; then
+                rm -rf $TARGET_ENV
+            fi;;
+        list | l[s] )
+            ls $PYVENV_ROOT;;
+        * )
+            if [ -d "$PYVENV_ROOT/$1" ]; then
+                source $PYVENV_ROOT/$1/bin/activate
+            else
+                cat << EOF
+usage: pyv [new <n> | activate <a> | deactivate <d> | remove <r> | ls <l>] [venv_name] [python-version]
+
+example:
+    pyv new ml 3.8.6
+    pyv activate ml
+    pyv deactivate
+    pyv remove ml
+    pyv ls
+EOF
+            fi
+    esac
+}
+
